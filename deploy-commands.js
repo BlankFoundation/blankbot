d111f2c90c8e6cdc86244db8b28f9b1e15deb9f1
Samantha Whitmore <whitmore.samantha@gmail.com>
2021-10-12 16:52:46 -0400

    Initial commit for whitelisting discord bot

---
+++ deploy-commands.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId, discordToken } = require('./config.json');
+
+const commands = [
+    new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+    new SlashCommandBuilder()
+      .setName('whitelist')
+      .setDescription('Adds a wallet address to the whitelist for minting')
+      .addStringOption(option =>
+        option.setName('address')
+	  .setDescription('Address of the Metamask wallet to add to the whitelist')
+          .setRequired(true))
+]
+    .map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(discordToken);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch(console.error);
---
+++ index.js
@@ -0,0 +1,59 @@
+const { airtableKey, airtableId, discordToken } = require('./config.json');
+
+// Import discord.js and create the client
+const { Client, Intents } = require('discord.js')
+var Airtable = require('airtable');
+var database = new Airtable({apiKey: airtableKey}).base(airtableId);
+const client = new Client({ 
+  intents: [Intents.FLAGS.GUILDS]
+ });
+    
+// Register an event so that when the bot is ready, it will log a messsage to the terminal
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+})
+    
+// Register an event to handle incoming messages
+client.on('message', async msg => {
+  // This block will prevent the bot from responding to itself and other bots
+  if(msg.author.bot) {
+    return
+  }
+    
+  // Check if the message starts with '!hello' and respond with 'world!' if it does.
+  if(msg.content.startsWith("!hello")) {
+    msg.reply("world!")
+  }
+})
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply({ content: 'Pong!', ephemeral: true });
+  } else if (interaction.commandName === 'whitelist') {
+    console.log(interaction);
+    const discordUserName = interaction.user.username;
+    const discordUserId = interaction.user.id;
+    const walletAddress = interaction.options.getString('address');
+    database('Whitelist').create([
+      {
+        "fields": {
+          "DiscordUserName": discordUserName,
+          "WalletAddress": walletAddress,
+          "DiscordUserId": discordUserId
+         }
+      }], function(err, records) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    records.forEach(function (record) {
+      console.log(record.getId());
+    });
+  });}
+});
+    
+// client.login logs the bot in and sets it up for use. You'll enter your token here.
+client.login(discordToken);
+
