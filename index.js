b0b16b35e29896a08e3fb6a6d2f5965d9506e1be
Samantha Whitmore <whitmore.samantha@gmail.com>
2021-10-14 14:37:53 -0400

    fix issues with async airtable query, add lazyminter lib

--- index.js
+++ index.js
@@ -26,16 +26,61 @@ client.on('message', async msg => {
   }
 })
 
-function findAirtableValue(airtableBase, airtableField, value) {
-    let recordsFound = database(airtableBase).select({
-        filterByFormula: `{${airtableField}} = ${value}`
-    }).firstPage(function(err,records) {
-        if(err) {
-            console.log(err);
-            return;
-        }
-        return recordsFound;
-    })
+//function discordUserIdExistsInDB(value) {
+//  const recordsExist = false;
+//  database('Whitelist').select({
+//    filterByFormula: "({DiscordUserId} = '" + value + "')"
+//  }).eachPage(function page(records, fetchNextPage) {
+//    if (records) {
+//      console.log('hi');
+//      recordsExist = true;
+//    }
+//    fetchNextPage();
+//  }, function done(error) {
+//    console.log(error);
+//  });
+//  return recordsExist;
+//}
+
+async function discordUserIdExistsInDB(value) {
+   let matchingRecords = [];
+   let recordsExist = false;
+   await database("WhiteList")
+  .select({
+    filterByFormula: "({DiscordUserId} = '" + value + "')"
+  })
+  .eachPage(
+    function page(records, fetchNextPage) {
+     try {
+      records.forEach(function(record) {
+        matchingRecords.push(record.id);
+      });
+      } catch(e){ console.log('error inside eachPage => ',e)}
+      fetchNextPage();
+    },
+    function done(err) {
+      if (err) {
+        console.error(err);
+      }
+    }
+  );
+  return (matchingRecords.length > 0);
+}
+
+function addRecord(discordUserName, walletAddress, discordUserId) {
+  database("Whitelist").create([
+    {
+      "fields": {
+        "DiscordUserName": discordUserName,
+        "WalletAddress": walletAddress,
+        "DiscordUserId": discordUserId
+       }
+    }], function(err, records) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
 }
 
 
@@ -45,35 +90,27 @@ client.on('interactionCreate', async interaction => {
   if (interaction.commandName === 'ping') {
     await interaction.reply({ content: 'Pong!'});
   } else if (interaction.commandName === 'whitelist') {
-    console.log(interaction);
-    if (interaction.member.roles.some(role => role.name === "member")) {
+    if (interaction.member.roles.cache.some(role => role.name === "member")) {
       const discordUserName = interaction.user.username;
       const discordUserId = interaction.user.id;
       const walletAddress = interaction.options.getString('address');
-      existingRecords = findAirtableValue("Whitelist", "DiscordUserId", discordUserId);
-      if (!existingRecords || (existingRecords.length == 0)) {
-        database('Whitelist').create([
-          {
-            "fields": {
-              "DiscordUserName": discordUserName,
-              "WalletAddress": walletAddress,
-              "DiscordUserId": discordUserId
-             }
-          }], function(err, records) {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        records.forEach(function (record) {
-          console.log(record.getId());
-        });
-      });
-      await interaction.reply({ content: 'Wallet address ' + walletAddress + ' added for member ' + discordUserName, ephemeral: true});
+      discordUserIdExists = await discordUserIdExistsInDB(discordUserId);
+      console.log(interaction);
+      if (discordUserIdExists) {
+        addRecord(discordUserName, walletAddress, discordUserId);
+        await interaction.reply(
+          { content: 'Wallet address ' + walletAddress + ' added for member ' + discordUserName,
+          ephemeral: true});
+      } else {
+        await interaction.reply(
+        { content: 'Wallet address already exists for member ' + discordUserName + '. Please contact a member of our moderation team to handle this issue!',
+          ephemeral: true});
+      }
     } else {
-      await interaction.reply({ content: 'Wallet address already exists for member ' + discordUserName + '. Please contact a member of our moderation team to handle this issue!', ephemeral: true});
-    }
-  } else {
-    await interaction.reply({ content: "You must be a member to add your wallet address to the whitelist. Apply in the #applications channel, we'd love to have you!", ephemeral: true});
+      await interaction.reply(
+        { content: "You must be a member to add your wallet address to the whitelist. Apply in the #applications channel, we'd love to have you!",
+          ephemeral: true });
+      }
   }
 });
     
