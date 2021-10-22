632624131ca09e18f759e2816038a13a61d2ad11
Jared Cosulich <jared.cosulich@gmail.com>
2021-10-22 08:43:02 -0400

    initial refactoring pass

---
+++ commands/index.js
@@ -0,0 +1,4 @@
+export { default as ping } from './ping.js'
+export { default as whoami } from './whoami.js'
+export { default as whitelist } from './whitelist.js'
+export { default as notfound } from './notfound.js'
---
+++ contracts/index.js
@@ -0,0 +1,1 @@
+export { default as BlankArt } from './BlankArt.js'
