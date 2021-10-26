5bd7b802143058b14129a440da291752238a3342
Samantha Whitmore <whitmore.samantha@gmail.com>
2021-10-26 15:28:27 -0700

    add test config with shape

---
+++ config.test.js
@@ -0,0 +1,16 @@
+export default {
+  "network": "ropsten",
+  "clientId": "test",
+  "guildId": "test",
+  "infuraProjectId": "test",
+  "infuraProjectSecret": "test",
+  "discordToken": "test",
+  "airtableKey": "test",
+  "airtableId": "test",
+  "foundationPrivateKey": "test",
+  "applicationChannel": 'bot-test',
+  "applicationChannelId": '897532024980643850',
+  "moderatorChannel": 'bot-test',
+  "moderatorChannelId": '897532024980643850',
+  "mintingUrl": "https://blank.foundation/mint"
+}
