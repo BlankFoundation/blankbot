5a9dde618fbea4c6716b750d862909b8bfc2d61c
Samantha Whitmore <whitmore.samantha@gmail.com>
2021-12-02 22:59:24 -0800

    this is spaghetti code but it worked

--- commands/reviewApplications.js
+++ commands/reviewApplications.js
@@ -76,36 +76,101 @@ async function getDiscordUserIdPresent(tableName, value) {
 }
 
 
+
+const messageThresholdPassed = async (message) => {
+  if (message.partial) {
+    await message.fetch()
+  }
+
+  // console.log(reaction.message.guild.members)
+
+
+  let memberIds = new Set();
+  let uniqueMemberReactions = 0;
+  let numDoubles = 0;
+  let councilMemberReaction = false;
+  //console.log(message);
+  const allReactions = await message.reactions.cache.map(r => r);
+  await message.guild.members.fetch()
+  for (var i = 0; i < allReactions.length; i++) {
+      const newReaction = allReactions[i];
+      let uniqueReaction = false;
+      const newUsers = await newReaction.users.fetch();
+      numDoubles = 0;
+      for (const [_key, user] of newUsers) {
+          var numUniques = 0;
+          const member = message.guild.members.resolve(user)?.roles?.cache?.find(
+              r => r.name === 'Member'
+          ) !== undefined;
+
+          const councilMember = message.guild.members.resolve(user)?.roles?.cache?.find(
+              r => r.name === 'Council'
+          ) !== undefined;
+          const contributor = message.guild.members.resolve(user)?.roles?.cache?.find(
+              r => r.name === 'Contributor'
+          ) !== undefined;
+          if ((councilMember) || (contributor)) {
+              councilMemberReaction = true;
+          }
+
+          if (!member) continue
+
+          if (!memberIds.has(user.id)) {
+              uniqueReaction = true;
+              numUniques++;
+          }
+          memberIds.add(user.id);
+      }
+      if (uniqueReaction) {
+          uniqueMemberReactions++;
+      }
+      if (numUniques > 1) {
+          numDoubles++;
+      }
+  }
+  var results = {
+    'numDoubles': numDoubles,
+    'uniqueMemberReactions': uniqueMemberReactions,
+    'hasCouncilMemberReaction': councilMemberReaction
+  }
+  console.log(results);
+
+  return results;
+}
+
+
 const reviewApplications = async (interaction) => {
     if ((interaction.member.roles.cache.some(role => role.name === "Council")) || (
         interaction.member.roles.cache.some(role => role.name === 'Contributor'))) {
-        const channel = interaction.client.channels.fetch(config.applicationChannelId);
+        const channel = interaction.client.channels.cache.get(config.applicationChannelId);
         const allMessages = await fetchAllMessages(channel)
         await channel.guild.members.fetch();
         for (var i = 0; i < allMessages.length; i++) {
+            console.log(i);
             let message = allMessages[i];
-            var isMember = message.guild.members.resolve(message.author)?.roles?.cache?.some(r => r.name === 'Member');
+            let fetchedMessage = await channel.messages.fetch(message[0]);
+            var isMember = await channel.guild.members.resolve(fetchedMessage.author)?.roles?.cache?.some(r => r.name === 'Member');
             if (!isMember) {
-                const whetherToHandle = await reactionThresholdPassed(reaction);
+                const whetherToHandle = await messageThresholdPassed(fetchedMessage);
                 var numDoubles = whetherToHandle['numDoubles'];
                 var uniqueMemberReactions = whetherToHandle['uniqueMemberReactions'];
-                var councilMemberReaction = whetherToHandle['hasCouncilMemberReaction'];
-            }
-            if ((uniqueMemberReactions >= 5) && (numDoubles < 5) && (councilMemberReaction)) {
-                let tableName = "ReadyForPromotion"
-                var userNamePresent = await getDiscordUserIdPresent(tableName, message.author.id);
-                if (!userNamePresent) {
-                    addReviewRecord(tableName, message.author.username,
-                        message.author.id, message.url,
-                        uniqueMemberReactions, message.timestamp)
-                }
-            } else if ((uniqueMemberReactions >= 5) && (numDoubles < 5) && (!councilMemberReaction)) {
-                let tableName = "CouncilContributorVoteNeeded";
-                var userNamePresent = await getDiscordUserIdPresent(tableName, message.author.id);
-                if (!userNamePresent) {
-                    addReviewRecord(tableName, message.author.username,
-                        message.author.id, message.url,
-                        uniqueMemberReactions, message.timestamp);
+                var councilMemberReaction = whetherToHandle['hasCouncilMemberReaction']
+                if ((uniqueMemberReactions >= 5) && (numDoubles < 5) && (councilMemberReaction)) {
+                    let tableName = "ReadyForPromotion"
+                    var userNamePresent = await getDiscordUserIdPresent(tableName, fetchedMessage.author.id);
+                    if (!userNamePresent) {
+                        addReviewRecord(tableName, fetchedMessage.author.username,
+                            fetchedMessage.author.id, fetchedMessage.url,
+                            uniqueMemberReactions, fetchedMessage.createdTimestamp)
+                    }
+                } else if ((uniqueMemberReactions >= 5) && (numDoubles < 5) && (!councilMemberReaction)) {
+                    let tableName = "CouncilContributorVoteNeeded";
+                    var userNamePresent = await getDiscordUserIdPresent(tableName, fetchedMessage.author.id);
+                    if (!userNamePresent) {
+                        addReviewRecord(tableName, fetchedMessage.author.username,
+                            fetchedMessage.author.id, fetchedMessage.url,
+                            uniqueMemberReactions, fetchedMessage.createdTimestamp);
+                    }
                 }
             }
         }
