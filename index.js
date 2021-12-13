773394f39dacdff11ad52220e1380d052fdfbeed
Samantha Whitmore <whitmore.samantha@gmail.com>
2021-12-13 08:39:06 -0700

    update channel links

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ client.on('guildMemberUpdate', async(oldMember, newMember) => {
   if ((!oldMember.roles.cache.some(role => role.name === "Member")) && (
       newMember.roles.cache.some(role => role.name === "Member"))) {
       const channel = client.channels.cache.find(channel => channel.id === config.welcomeChannelId);
-      channel.send(`Applicant ${newMember} has been promoted to Blank member! Welcome! Feel free to look around, ask questions in #no-silly-questions and browse the resources in #links!`);
+      channel.send(`Applicant ${newMember} has been promoted to Blank member! Welcome! Feel free to look around, ask questions in #◽〡no-silly-questions and browse the resources in #◽〡links!`);
   }
 });
 
